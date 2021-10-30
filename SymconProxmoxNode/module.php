acd3447bd80954e20edda50ae5e4da62f946a6bb
IPSAttain <35669489+IPSAttain@users.noreply.github.com>
2021-10-30 18:23:54 +0200

    First Test Version
    Test Version, um den Datenfluss zwischen den Modulen herzustellen.

--- SymconProxmoxNode/module.php
+++ SymconProxmoxNode/module.php
@@ -27,17 +27,33 @@ declare(strict_types=1);
 			parent::ApplyChanges();
 		}
 		
-		public function Send()
+		public function Update()
 		{
+			// this function is called, to get data from the I/O
+			$data = 'Give me Data';
 
-			return $this->SendDataToParent(json_encode(['DataID' => '{A8F28772-131A-351B-DEC1-BA2995EAE4C3}']));
 
-			
+			$debug = 'I send to the I/O: ' . $data;
+			$this->SendDebug(__FUNCTION__,  $debug , 0);
+
+			$return = $this->Send_To_Parent($data);
+
+			$debug= 'I got from the I/O: ' . $return;
+			$this->SendDebug(__FUNCTION__,  $debug , 0);
 		}
 
 		public function ReceiveData($JSONString)
 		{
 			$data = json_decode($JSONString);
 			IPS_LogMessage('Device RECV', utf8_decode($data->Buffer));
+		}
+
+		protected function Send_to_Parent($Buffer)
+		{
+			$return = $this->SendDataToParent(json_encode([
+				'DataID' => "{A8F28772-131A-351B-DEC1-BA2995EAE4C3}",
+				'Buffer' => utf8_encode($Buffer),
+			]));
+			return $return;
 		}
 	}
